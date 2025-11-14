e72aed964360a6f8b8b717b3775a66c467345cee
SivakumarM-ks005 <sivakumar.mani@kryasolutions.com>
2025-11-14 21:33:06 +0530

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,9 +3,13 @@ const connection = require('../connection');
 const signup = async(req,res )=>{
 
         const user = req.body;
-        query = "select * from user where userName=?";
-        connection.query(query,[user.userName], (error, results)=>{
+        query = "select * from user where userName=? and email=?";
+        connection.query(query,[user.userName, user.email], (error, results)=>{
             try {
+                console.log("result", results[0]);
+                if(results.length > 0){
+                    
+                }
                 if(results.length <= 0){
                       query ="insert into user ( username, password, email, firstName, lastName, dateRegistered,lastLogin,role,Status ) values (?,?,?,?,?,'2025-11-10', '2025-11-10', 'user','false')";
                        connection.query(query, [user.userName, user.password, user.email, user.firstName, user.lastName],(error, results)=>{
